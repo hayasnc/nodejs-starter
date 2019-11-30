1efceb70ed531f8e69a0bd6913ec616b65692e48
hayas <hayas@qburst.com>
2019-11-30 09:58:26 +0530

    Node Express Postgres Setup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../database');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+});
+
+router.post('/', (request, response) => {
+  const { name, email } = request.body;
+
+  pool.query(
+    'INSERT INTO users (name, email) VALUES ($1, $2)',
+    [name, email],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${result.insertId}`);
+    }
+  );
+});
+
+router.get('/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+});
+
+router.put('/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, email } = request.body;
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+});
+
+router.delete('/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
 });
 
 module.exports = router;
